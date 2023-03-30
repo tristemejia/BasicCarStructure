e6373acd531b671db12f531f210eff7dd573759b
tristemejia <anibarmejia@gmail.com>
2023-03-30 12:02:47 -0400

    the basic structure of thye car is ready

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BasicStructure audi =new BasicStructure();
+        audi.color="balck";
+        audi.model="A3";
+        audi.power="5000cc";    }
+}
